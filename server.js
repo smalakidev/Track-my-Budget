e709e49152dcdff365e6083746afadee0ef68b67
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-05-02 13:45:43 -0400

    added a git ignore and modified alot of code in the server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+//imports the env file and uses it
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,10 +17,23 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+//Connection for MongoDB code just added. So it works fully on the DB
+
+mongoose.connect(
+  process.env.MONGODB_URI || `mongodb://localhost/budget`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 
 // routes
 app.use(require("./routes/api.js"));
@@ -26,3 +41,4 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
